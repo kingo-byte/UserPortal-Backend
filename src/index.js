b17f4a222c8633845a3b9bc206cbc85cd7d3a154
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-09-16 15:46:26 +0300

    removed useless comment

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//use cors to allow any
-
 app.get("/", (req, res) => {
     res.send ("Welcome to User Portal Backend");    
 });
