fbb476defcc58983394981f617cdde525e3c045d
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-09-17 21:37:33 +0300

    configure cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+    origin: 'http://localhost:4200', // Allow only this origin
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'], // Allowed methods
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    optionsSuccessStatus: 200 // Some legacy browsers choke on status 204
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send ("Welcome to User Portal Backend");    
@@ -24,24 +32,24 @@ app.get("/api/users", async (req, res) => {
     
     return res.json(result);
 });
+
 app.post("/api/users", async (req, res) => {
     const user = req.body;
     user._id = uuidv4();
     await insertUser(user);
-
-    return JSON.parse("Ok");
+    
+    return res.status(201).json({ message: "User created successfully" });
 });
 
 app.delete("/api/users/:id", async (req, res) => {
     const id = req.params.id;
     await deleteUser(id);
-    return JSON.parse("Ok");
+    return res.status(200).json({ message: "User deleted successfully" });
 });
 
 app.patch("/api/users", async (req, res) => {
     const user = req.body;
     await updateUser(user);
-    return JSON.parse("Ok");
-})
-  
+    return res.status(200).json({ message: "User updated successfully" });
+});
 
